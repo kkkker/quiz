89de5443b562de983aadbc506dbda2c1a19877bf
kker <907765631@qq.com>
2020-09-25 14:51:03 +0800

    [Mamingjun]: implement css of nav bar

--- front-end/src/nav/nav.js
+++ front-end/src/nav/nav.js
@@ -1,29 +1,54 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { FiHome, FiShoppingCart, FiPlus } from "react-icons/fi";
+import "./nav.css";
 
 class Nav extends Component {
   render() {
     return (
-      <nav>
-        <NavLink to="/">
-          <div>
-            <FiHome className="store-icon icon" />
-            <p>商城</p>
-          </div>
-        </NavLink>
-        <NavLink to="/order">
-          <div>
-            <FiShoppingCart className="store-icon icon" />
-            <p>订单</p>
-          </div>
-        </NavLink>
-        <NavLink to="/order">
-          <div>
-            <FiPlus className="store-icon icon" />
-            <p>添加商品</p>
-          </div>
-        </NavLink>
+      <nav className="nav">
+        <div className="link">
+          <NavLink
+            exact
+            activeStyle={{
+              backgroundColor: "#1690FF",
+            }}
+            to="/"
+          >
+            <div>
+              <FiHome className="store-icon icon" />
+              <p>商城</p>
+            </div>
+          </NavLink>
+        </div>
+        <div className="link">
+          <NavLink
+            exact
+            activeStyle={{
+              backgroundColor: "#1690FF",
+            }}
+            to="/order"
+          >
+            <div>
+              <FiShoppingCart className="store-icon icon" />
+              <p>订单</p>
+            </div>
+          </NavLink>
+        </div>
+        <div className="link">
+          <NavLink
+            exact
+            activeStyle={{
+              backgroundColor: "#1690FF",
+            }}
+            to="/addProduct"
+          >
+            <div>
+              <FiPlus className="store-icon icon" />
+              <p>添加商品</p>
+            </div>
+          </NavLink>
+        </div>
       </nav>
     );
   }
