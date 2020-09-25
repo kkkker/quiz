d056cdf323fc9e09e1638c644a89377be4de0891
kker <907765631@qq.com>
2020-09-25 18:23:22 +0800

    [Mamingjun]: implement no order

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Nav from "./nav/nav";
 import Store from "./store/store";
 import Order from "./order/order";
@@ -8,15 +8,16 @@ import AddProducts from "./add-products/add_products";
 import Footer from "./footer/footer";
 
 class App extends Component {
-
   render() {
     return (
       <BrowserRouter>
         <div className="app">
           <Nav className="nav-bar" />
-          <Route exact path="/" component={Store} />
-          <Route exact path="/order" component={Order} />
-          <Route exact path="/addProducts" component={AddProducts} />
+          <Switch>
+            <Route exact path="/order" component={Order} />
+            <Route exact path="/addProducts" component={AddProducts} />
+            <Route exact path="/" component={Store} />
+          </Switch>
           <Footer className="footer" />
         </div>
       </BrowserRouter>
--- front-end/src/order/order.js
+++ front-end/src/order/order.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./order.css";
 
 class Order extends Component {
   render() {
     let orders = window.orders;
-    if (orders.length === 0) {
+    if (orders === undefined || orders.length === 0) {
+      console.log(1);
       return (
-        <div className="order">
+        <div className="no-order">
           <p>
-            暂无订单，返回<Link>商城页面</Link>继续购买
+            暂无订单，返回<Link to="/">商城页面</Link>继续购买
           </p>
         </div>
       );
     }
+    console.log(2);
     return <div className="order">Order</div>;
   }
 }
