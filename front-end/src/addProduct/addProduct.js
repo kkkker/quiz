f8dc646eccca0bdea70324d594620165e91e035a
kker <907765631@qq.com>
2020-10-23 11:33:08 +0800

    [Mamingjun] feat: implement add prodect html

--- front-end/src/addProduct/addProduct.js
+++ front-end/src/addProduct/addProduct.js
@@ -1,9 +1,78 @@
 import React, { Component } from "react";
+import "./addProduct.css"
 
 class AddProduct extends Component {
+  state = {
+    name: "",
+    price: 0,
+    units: "",
+    imageUrl: "",
+  };
+
   render() {
     return (
-      <div className="add-product"><p>AddProduct</p></div>
+      <div className="add-product">
+        <form>
+          <h2>添加商品</h2>
+          <h6>
+            <span>*</span>名称:
+          </h6>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="名称"
+            value={this.state.name}
+            onChange={(e) => this.handleChange("name", e)}
+            id="name"
+          />
+          <h6>
+            <span>*</span>价格:
+          </h6>
+          <input
+            type="number"
+            className="form-control"
+            placeholder="价格"
+            value={this.state.name}
+            onChange={(e) => this.handleChange("price", e)}
+            id="price"
+          />
+          <h6>
+            <span>*</span>单位:
+          </h6>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="单位"
+            value={this.state.name}
+            onChange={(e) => this.handleChange("units", e)}
+            id="units"
+          />
+          <h6>
+            <span>*</span>图片:
+          </h6>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="图片"
+            value={this.state.name}
+            onChange={(e) => this.handleChange("imageUrl", e)}
+            id="imageUrl"
+          />
+          <button
+            type="submit"
+            disabled={
+              !this.state.name ||
+              !this.state.price ||
+              !this.state.units ||
+              !this.state.imageUrl
+            }
+            className="btn btn-primary"
+            id="submit-button"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     );
   }
 }
